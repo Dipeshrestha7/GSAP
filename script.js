deba2be87a759cd960c52a0aadafd27217d6a22a
Dipesh Shrestha <dipstha061@gmail.com>
2025-09-27 13:52:46 +0545

    basic gsap

---
+++ script.js
@@ -0,0 +1,50 @@
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"100%",
+//     repeat:"-1",
+//     yoyo:true
+// })
+
+// gsap.from("#box2",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"0%",
+//     repeat:"-1",
+//     yoyo:true
+// })
+
+// gsap.from("h1",{
+//     opacity:0,
+//     color:"red",
+//     duraation:5,
+//     delay:1,
+//     stagger:0.3,
+// })
+
+
+var tl = gsap.timeline()
+
+tl.to("#box",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"100%",
+})
+
+tl.to("#box2",{
+    x:1000,
+    duration:2,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"0%",
+
+})
